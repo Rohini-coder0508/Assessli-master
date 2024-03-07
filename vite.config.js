f340e0b48df5ed4fff72a1c3223ce35a798b5621
Rohini Mishra <114797468+Rohini-coder0508@users.noreply.github.com>
2024-03-07 21:32:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 8080,
-//     proxy: {
-//       '/api': {
-//         // target: 'https://assessli-chi.vercel.app/https://script.google.com',
-//         target: 'http://localhost:8080/https://script.google.com',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     },
-//   },
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -34,7 +7,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/https://script.google.com/macros/s/AKfycbyJHMSe5UnkRS07JZGb5hsmLWFfECNOUaZsZ7ZWyrQ726ktWvQhVyz9xQAU9R_O5AmstA/exec', // Update this to the actual API endpoint
+        target: 'http://localhost:8080/https://script.google.com/macros/s/AKfycbw3A0cxmWzVZ0zio290G_NeRdG49p-TyIN_pMrK0ayohy-Ij1HHj6hgfj0i7zUJVGVx/exec',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
